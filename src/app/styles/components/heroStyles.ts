889c901f4b703c40d679b9b04f63d4746a91879c
Marnien Cueba <marniencueba94@gmail.com>
2023-10-26 06:06:51 +0800

    styles

--- src/app/styles/components/heroStyles.ts
+++ src/app/styles/components/heroStyles.ts
@@ -1,10 +1,34 @@
 export const heroStyles = {
   root: {
-    base: "!h-[810px] bg-warning bg-no-repeat bg-cover bg-center before:content-[''] before:absolute before:inset-0 before:backdrop-blur-md before:bg-white/50",
+    base: "!h-[810px] bg-warning bg-no-repeat bg-cover bg-center before:content-[''] before:absolute before:inset-0 before:backdrop-blur-md before:bg-white/50 duration-200 ease-in transition-all",
   },
   container: {
-    base: "!h-full grid grid-cols-2 gap-16",
+    base: "!h-full grid grid-cols-1 lg:grid-cols-2 gap-16",
   },
+  columns: {
+    base: "relative flex items-center overflow-hidden",
+    image: {
+      base: "w-full h-full bg-warning object-cover rounded-xl",
+      owner: {
+        base: "absolute bottom-1 right-1 text-white/10",
+      },
+    },
+    details: {
+      base: "space-y-12",
+      title: {
+        base: "space-y-4 font-bold text-left tracking-wide",
+        first_line: "block text-3xl",
+        last_line: "block text-5xl",
+      },
+      message: {
+        base: "text-xl font-medium",
+      },
+      cta: {
+        base: "hover:gap-4",
+      },
+    },
+  },
+
   column: {
     details: {
       base: "flex items-center",
--- src/app/styles/components/index.ts
+++ src/app/styles/components/index.ts
@@ -1,3 +1,4 @@
 export { iconStyles } from "./iconStyles";
+export { iconFontStyles } from "./iconFontStyles";
 export { buttonStyles } from "./buttonStyles";
 export { heroStyles } from "./heroStyles";
